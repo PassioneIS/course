d998a5f6623cb6b98b199c32bdff02e9e9b587c1
Santiago Maldonado <samaldonado@unal.edu.co>
2025-07-18 03:45:41 -0500

    feat: Muestra datos reales de la BD en el feed, incluyendo calificación

--- Proyecto/CookTrack/src/main/java/services/FeedService.java
+++ Proyecto/CookTrack/src/main/java/services/FeedService.java
@@ -1,19 +1,37 @@
 package services;
 
+import dao.impl.PostDaoImpl;
+import dao.interfaces.PostDao;
 import models.Ingredient;
 import models.Post;
 import models.Recipe;
 import models.User;
 
+import java.util.Collections;
 import java.util.List;
 
 public class FeedService {
+
+    private final PostDao postDao;
+
+
+    public FeedService() {
+        this.postDao = new PostDaoImpl();
+    }
+
     public void publish(User user, Recipe recipe) {
 
     }
 
     public List<Post> getPosts(User user) {
-        return null;
+
+        try {
+            return postDao.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return Collections.emptyList();
+        }
     }
 
     public void importRecipe(Recipe recipe) {
