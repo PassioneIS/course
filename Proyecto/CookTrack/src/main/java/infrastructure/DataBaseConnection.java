cef9e72d8464131fce676890c6cc768e65dc2129
Maicol Garzón <mgarzonm@unal.edu.co>
2025-07-22 16:37:35 -0500

    DataBaseConnection Fix

--- Proyecto/CookTrack/src/main/java/infrastructure/DataBaseConnection.java
+++ Proyecto/CookTrack/src/main/java/infrastructure/DataBaseConnection.java
@@ -3,6 +3,9 @@ package infrastructure;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class DataBaseConnection {
 
@@ -14,7 +17,13 @@ public class DataBaseConnection {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+                Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+                configuration.addProperties(new Properties() {{
+                    try (InputStream input = new FileInputStream("config/hibernate.properties")) {
+                        load(input);
+                    }
+                }});
+                sessionFactory = configuration.buildSessionFactory();
             } catch (Throwable ex) {
                 System.err.println("Falló la creación del SessionFactory: " + ex);
                 throw new ExceptionInInitializerError(ex);
